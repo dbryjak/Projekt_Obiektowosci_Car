360f54429e352c71ffd7ee2bd9c02a8b6abb575f
DanielBryjak <d.bryjak@op.pl>
2022-10-30 07:59:29 +0100

    Obiektowość - Frogs

--- src/Frog.java
+++ src/Frog.java
@@ -1,14 +1,24 @@
 public class Frog {
-    int longth; // INT - cyfry
-    String name; // zmienna literowa
-    boolean softness; // boolean - zmienna
+    // wypisane wszystkie PARAMETRY
+    private int longth;         // INT - cyfry
+    private String name;        // String - zmienna literowa
+    private boolean softness;   // boolean - zmienna
+                                // private - enkapsulacja, czyli zamknięcie dostępu do naszej klasy
 
+    // KONSTRUKTOR
     Frog (int longth, String name, boolean softness) {
         this.longth = longth;
         this.name = name;
         this.softness = softness;
-
-
     }
-
+    // METODA - 'changeName'
+    // int - zwraca wartość liczbową,
+    // String - zwraca nazwę,
+    // String [] - zwraca tablicę Stringów,
+    // void (pustka) - modyfikator dostępu publicznego - niczego nie zwraca, tylko nadaje nowe imię -
+    // tzn. nadaje nową wartość w 'name'
+    // chengeName - zmiana imienia
+    public void changeName(String name){
+        this.name = name;
+    }
 }
--- src/Frog_Main.java
+++ src/Frog_Main.java
@@ -2,6 +2,6 @@ public class Frog_Main {
     public static void main(String[] args) {
 
         Frog frog1= new Frog(10, "Kermit", true);
-
+        frog1.changeName("Bolo");
     }
 }
