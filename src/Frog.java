8f41612a6191534c9a0fd466996799d83a67f838
DanielBryjak <d.bryjak@op.pl>
2022-10-30 11:56:49 +0100

    Obiektowość - Frogs

--- src/Frog.java
+++ src/Frog.java
@@ -11,12 +11,13 @@ public class Frog {
         this.name = name;
         this.softness = softness;
     }
-    // METODA - 'changeName'
+    // METODA - 'change...'
     // int - zwraca wartość liczbową,
     // String - zwraca nazwę,
     // String [] - zwraca tablicę Stringów,
     // void (pustka) - modyfikator dostępu publicznego - niczego nie zwraca, tylko nadaje nowe imię -
     // tzn. nadaje nową wartość w 'name'
+    //---------------------------------------------------------------------------------------------
     // chengeName - zmiana imienia
     public void changeName(String name){
         this.name = name;
@@ -24,6 +25,37 @@ public class Frog {
     // viewName - metoda, która pokazuje zmianę (np. imienia)
     public String viewName(){
         return name;
-
+        }
+    //--------------------------------------------------------------------------------------------
+    // changeSoftnes - analogicznie
+    public void changeSoftness(boolean softness) {
+        this.softness = softness;
+        }
+    // viewSoftnes - metoda, która ukazuje zmianę softnes'a
+    public boolean viewSoftness(){
+        return softness;
     }
+    // -------------------------------------------------------------------------------------------
+    // changeLonght - analogicznie
+    public void changeLonght(int longth){
+        this.longth = longth;
+    }
+    public int viewLongth() {
+        return longth;
+    }
+    // ------------------------------------------------------------------------------------------
+    //
+    public double convert(int longht){
+        double cale = longht*2.54; // double - zmienne przecinkowe
+        return cale;
+    }
+
 }
+
+// od linka 14 do 51 włącznie, wszystko usunąć.
+// następnie z tego co pozostało, przed ostatniki wąsami '}'
+// kliknąć prawym przyciskien i klik 'Generate...'
+// wybrać 'Construktor' a następnie zaznaczyćłącznie wszystkie pozycje (bez Frog) i OK
+// klik w link pod } Construktor i znowu prawy przysisk i wybrać 'Generate' i nastęnie
+// Getter and Setter.
+// i wszystko tworzy się samo :)
--- src/Frog_Main.java
+++ src/Frog_Main.java
@@ -2,12 +2,24 @@ public class Frog_Main {
     public static void main(String[] args) {
 
         Frog frog1= new Frog(10, "Kermit", true);
+        // --- zmiana imienia ----------
         // najpierw wymusić zmianę imienia - metodą 'changeName' - przebiega w tle, niewidoczna,
         frog1.changeName("Bolo");
         // program już to widzi - metoda 'viewName' - my jeszcze nie,
         frog1.viewName();
         // ...println - pokazuje nam.
         System.out.println(frog1.viewName());
-
+        // --- zmiana softness'a ----------
+        frog1.changeSoftness(false);
+        frog1.viewSoftness();   // --- Ten link, jest zbędny
+        System.out.println(frog1.viewSoftness());
+        // --- zmiana longht ------------
+        frog1.changeLonght(15);
+        frog1.viewLongth();   // --- Ten link, jest zbędny
+        System.out.println(frog1.viewLongth());
+        // --- zmiana wcześniejszej zmiany 'longht', na długość w 'calach' - zczytuje dane z linka 19.
+        System.out.println(frog1.convert(frog1.viewLongth()));
+        frog1.convert(30);
+        System.out.println();
     }
 }
