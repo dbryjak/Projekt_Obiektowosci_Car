b1be2c04da47a180a888a1b5bac7a96cb5e76043
DanielBryjak <d.bryjak@op.pl>
2022-10-30 06:01:28 +0100

    Obiektowość - Frogs

---
+++ src/Frog.java
@@ -0,0 +1,14 @@
+public class Frog {
+    int longth; // INT - cyfry
+    String name; // zmienna literowa
+    boolean softness; // boolean - zmienna
+
+    Frog (int longth, String name, boolean softness) {
+        this.longth = longth;
+        this.name = name;
+        this.softness = softness;
+
+
+    }
+
+}
---
+++ src/Frog_Main.java
@@ -0,0 +1,7 @@
+public class Frog_Main {
+    public static void main(String[] args) {
+
+        Frog frog1= new Frog(10, "Kermit", true);
+
+    }
+}
