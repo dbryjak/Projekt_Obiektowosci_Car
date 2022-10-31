4b592a937bc4ac0dbb0d0b887b1ba67c66ca4b59
DanielBryjak <d.bryjak@op.pl>
2022-10-31 18:20:44 +0100

    aby wyświetliło opis + zmienna

---
+++ src/zmienna_z_napisem.java
@@ -0,0 +1,8 @@
+public class zmienna_z_napisem {
+    // aby wyświetliło opis + zmienna
+    public static void main(String[] args) {
+        int liczba = 100;
+        int liczba1 = 50;
+        System.out.println("Pierwsza wartość " +liczba);
+    }
+}
