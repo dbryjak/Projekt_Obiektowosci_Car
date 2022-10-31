974210eefd4b3aa6529861a333d09830193e1e2a
DanielBryjak <d.bryjak@op.pl>
2022-10-31 17:44:41 +0100

    aby wyświetliło zmienną

--- src/nazwa_w_cudzyslowiu.java
+++ src/nazwa_w_cudzyslowiu.java
@@ -1,4 +1,4 @@
-public class Notatki_wlasne {
+public class nazwa_w_cudzyslowiu {
     // aby wyświetlić napis w programie należy go umieścić w cudzysłowiu
     public static void main(String[] args) {
         System.out.println("\"Witam Towaszyszów\"");
