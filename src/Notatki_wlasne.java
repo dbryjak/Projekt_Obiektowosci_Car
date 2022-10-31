a7f293fbedcefba423b5940a94a383aa2481624d
DanielBryjak <d.bryjak@op.pl>
2022-10-31 17:39:38 +0100

    aby wyświetliło zmienną

---
+++ src/Notatki_wlasne.java
@@ -0,0 +1,7 @@
+public class Notatki_wlasne {
+    // aby wyświetlić napis w programie należy go umieścić w cudzysłowiu
+    public static void main(String[] args) {
+        System.out.println("\"Witam Towaszyszów\"");
+    }
+}
+
---
+++ src/wyswietlenie_zmiennej.java
@@ -0,0 +1,7 @@
+public class wyswietlenie_zmiennej {
+    // aby wyświetliło zmienną
+    public static void main(String[] args) {
+    int liczba = 100;
+    System.out.println(liczba);
+        }
+    }
